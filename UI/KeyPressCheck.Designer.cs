9222994ee9a67b297d5ab4a65d99765ff3f4bdb2
JoShMiQueL <joshmiquel.cs@gmail.com>
2025-11-20 22:23:10 +0100

    refactor: update KeyPressCheck form designer properties
    - Update form properties: add ControlBox=false, FormBorderStyle=FixedToolWindow, Icon resource
    - Adjust control sizing and positioning (AutoScaleDimensions, ClientSize, label Location/Size)
    - Add form icon resource data to resx file

--- UI/KeyPressCheck.Designer.cs
+++ UI/KeyPressCheck.Designer.cs
@@ -21,49 +21,50 @@ namespace TarkovPriceViewer.UI
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 45);
-            this.label1.Name = "label1";
-            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.label1.Size = new System.Drawing.Size(64, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Press Key";
-            // 
-            // KeyPressCheck
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(120, 97);
-            this.Controls.Add(this.label1);
-            this.Name = "KeyPressCheck";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Form1";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.KeyPressCheck_FormClosed);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyPressCheck_KeyUp);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KeyPressCheck));
+			label1 = new System.Windows.Forms.Label();
+			SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+			label1.AutoSize = true;
+			label1.Location = new System.Drawing.Point(31, 56);
+			label1.Name = "label1";
+			label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			label1.Size = new System.Drawing.Size(56, 15);
+			label1.TabIndex = 0;
+			label1.Text = "Press Key";
+			// 
+			// KeyPressCheck
+			// 
+			AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			ClientSize = new System.Drawing.Size(120, 121);
+			ControlBox = false;
+			Controls.Add(label1);
+			FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+			Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			Name = "KeyPressCheck";
+			StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			FormClosed += KeyPressCheck_FormClosed;
+			KeyUp += KeyPressCheck_KeyUp;
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        }
+		#endregion
 
-        #endregion
-
-        private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label1;
     }
 }
