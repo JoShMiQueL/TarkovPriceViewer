256f31209d599bf11f121317ea6afde2d7c28e03
JoShMiQueL <joshmiquel.cs@gmail.com>
2025-12-01 01:52:35 +0100

    feat: add global hotkey support for overlay show/hide functionality
    - Add Win32 P/Invoke declarations for RegisterHotKey and UnregisterHotKey
    - Register global hotkeys in OnSourceInitialized using ShowOverlayKey and HideOverlayKey from settings
    - Add WndProc message handler to intercept WM_HOTKEY messages and trigger overlay actions
    - Implement ShowOverlay and HideOverlay methods to retrieve OverlayWindow from DI container
    - Unregister hotkeys and remove message hook in OnClosed to prevent resource

--- TarkovPriceViewer/UI/MainWindow.xaml.cs
+++ TarkovPriceViewer/UI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,7 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Interop;
 using TarkovPriceViewer.Services;
+using TarkovPriceViewer.UI;
 using TarkovPriceViewer.Utils;
 
 namespace TarkovPriceViewer
@@ -21,6 +25,16 @@ namespace TarkovPriceViewer
         private readonly ISettingsService _settingsService;
         private readonly ITarkovDataService _tarkovDataService;
         private readonly ITarkovTrackerService _tarkovTrackerService;
+
+        private const int HotkeyIdShowOverlay = 1;
+        private const int HotkeyIdHideOverlay = 2;
+        private HwndSource _hwndSource;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 
         public MainWindow(
             ISettingsService settingsService,
@@ -40,6 +54,96 @@ namespace TarkovPriceViewer
 
             var version = VersionHelper.GetDisplayVersion();
             Title = $"Tarkov Price Viewer v{version}";
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            _hwndSource = (HwndSource)PresentationSource.FromVisual(this);
+            if (_hwndSource != null)
+            {
+                _hwndSource.AddHook(WndProc);
+
+                var showKey = _settingsService.Settings.ShowOverlayKey;
+                var hideKey = _settingsService.Settings.HideOverlayKey;
+
+                if (Enum.TryParse(showKey, out Key showParsed))
+                {
+                    var vk = (uint)KeyInterop.VirtualKeyFromKey(showParsed);
+                    RegisterHotKey(_hwndSource.Handle, HotkeyIdShowOverlay, 0, vk);
+                }
+
+                if (Enum.TryParse(hideKey, out Key hideParsed))
+                {
+                    var vk = (uint)KeyInterop.VirtualKeyFromKey(hideParsed);
+                    RegisterHotKey(_hwndSource.Handle, HotkeyIdHideOverlay, 0, vk);
+                }
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            if (_hwndSource != null)
+            {
+                UnregisterHotKey(_hwndSource.Handle, HotkeyIdShowOverlay);
+                UnregisterHotKey(_hwndSource.Handle, HotkeyIdHideOverlay);
+                _hwndSource.RemoveHook(WndProc);
+            }
+
+            base.OnClosed(e);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            if (msg == WM_HOTKEY)
+            {
+                int id = wParam.ToInt32();
+                if (id == HotkeyIdShowOverlay)
+                {
+                    ShowOverlay();
+                    handled = true;
+                }
+                else if (id == HotkeyIdHideOverlay)
+                {
+                    HideOverlay();
+                    handled = true;
+                }
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private void ShowOverlay()
+        {
+            var overlay = App.Host.Services.GetService(typeof(OverlayWindow)) as OverlayWindow;
+            if (overlay == null)
+            {
+                return;
+            }
+
+            if (!overlay.IsVisible)
+            {
+                overlay.Show();
+            }
+
+            overlay.Topmost = true;
+            overlay.Activate();
+        }
+
+        private void HideOverlay()
+        {
+            var overlay = App.Host.Services.GetService(typeof(OverlayWindow)) as OverlayWindow;
+            if (overlay == null)
+            {
+                return;
+            }
+
+            if (overlay.IsVisible)
+            {
+                overlay.Hide();
+            }
         }
     }
 }
